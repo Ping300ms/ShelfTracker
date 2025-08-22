1dc3e14ed6ac232337c3c68bb96b9cbfef66e2bd
pierre.maunier <pierre.maunier@epita.fr>
2025-08-22 18:58:07 +0200

    refacto(camera): created camera component to lighten ScanScreen

---
+++ shelftracker/src/components/ScanScreen/Camera.tsx
@@ -0,0 +1,44 @@
+// src/screens/ScanScreen.tsx
+import { useEffect, useRef, useState } from "react";
+import { Html5Qrcode } from "html5-qrcode";
+import "../../styles/Camera.css";
+
+function Camera() {
+    const scannerRef = useRef<Html5Qrcode | null>(null);
+    const [scannedCode, setScannedCode] = useState<string | null>(null);
+
+    useEffect(() => {
+        const scannerId = "reader";
+        const html5QrCode = new Html5Qrcode(scannerId, false);
+        scannerRef.current = html5QrCode;
+
+        html5QrCode
+            .start(
+                { facingMode: "environment" },
+                { fps: 10, qrbox: 250, aspectRatio: window.innerWidth / window.innerHeight },
+                (decodedText) => {
+                    setScannedCode(decodedText);
+                },
+                () => {}
+            )
+            .catch((err) => console.error("Impossible de démarrer le scanner :", err));
+
+        return () => {
+            if (scannerRef.current?.getState().toString() === "SCANNING") {
+                scannerRef.current
+                    .stop()
+                    .then(() => scannerRef.current?.clear())
+                    .catch((err) => console.error("Erreur lors de l'arrêt :", err));
+            }
+        };
+    }, []);
+
+    return (
+        <div className="camera-container">
+            <div id="reader"></div>
+            {scannedCode && <div className="scanned-code">QR Code : {scannedCode}</div>}
+        </div>
+    );
+}
+
+export default Camera;
--- shelftracker/src/screens/ScanScreen.tsx
+++ shelftracker/src/screens/ScanScreen.tsx
@@ -1,42 +1,11 @@
-// src/screens/ScanScreen.tsx
-import { useEffect, useRef, useState } from "react";
-import { Html5Qrcode } from "html5-qrcode";
-import "../styles/ScanScreen.css";
+import "../styles/Camera.css";
+import Camera from "../components/ScanScreen/Camera.tsx";
 
 function ScanScreen() {
-    const scannerRef = useRef<Html5Qrcode | null>(null);
-    const [scannedCode, setScannedCode] = useState<string | null>(null);
-
-    useEffect(() => {
-        const scannerId = "reader";
-        const html5QrCode = new Html5Qrcode(scannerId, false);
-        scannerRef.current = html5QrCode;
-
-        html5QrCode
-            .start(
-                { facingMode: "environment" },
-                { fps: 10, qrbox: 250, aspectRatio: window.innerWidth / window.innerHeight },
-                (decodedText) => {
-                    setScannedCode(decodedText);
-                },
-                () => {}
-            )
-            .catch((err) => console.error("Impossible de démarrer le scanner :", err));
-
-        return () => {
-            if (scannerRef.current?.getState().toString() === "SCANNING") {
-                scannerRef.current
-                    .stop()
-                    .then(() => scannerRef.current?.clear())
-                    .catch((err) => console.error("Erreur lors de l'arrêt :", err));
-            }
-        };
-    }, []);
 
     return (
-        <div className="scan-screen">
-            <div id="reader"></div>
-            {scannedCode && <div className="scanned-code">QR Code : {scannedCode}</div>}
+        <div>
+            <Camera/>
         </div>
     );
 }
