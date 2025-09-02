5a260925b07e5d1297185c5c66da4d04748a24b5
pierre.maunier <pierre.maunier@epita.fr>
2025-09-02 20:36:15 +0200

    feat: added equipment deletion

--- shelftracker/src/screens/EquipmentDetailScreen.tsx
+++ shelftracker/src/screens/EquipmentDetailScreen.tsx
@@ -1,12 +1,12 @@
-import { useParams } from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import TopBar from "../components/common/TopBar.tsx";
-import {getEquipmentById} from "../api/EquipmentsApi.ts";
+import {deleteEquipment, getEquipmentById} from "../api/EquipmentsApi.ts";
 import {useEffect, useState} from "react";
 import type {Equipment} from "../types/Equipment.ts";
 import "../styles/EquipmentDetail.css";
 import {Loader} from "../components/common/Loader.tsx";
 import {typeConfig} from "../utils/equipmentTypeConfig.ts";
-import {IoAdd, IoRemove} from "react-icons/io5";
+import {IoAdd, IoRemove, IoTrashOutline} from "react-icons/io5";
 import {useCart} from "../hooks/CartHook.ts";
 import EquipmentQR from "../components/common/EquipmentQR.tsx";
 
@@ -16,6 +16,8 @@ function EquipmentDetailScreen() {
     const [addedToCard, setAddedToCart] = useState(false);
     const { id } = useParams();
     const { addToCart, removeFromCart, cart } = useCart();
+
+    const navigate = useNavigate();
 
     const handleClick = () => {
         if (!equipment)
@@ -27,6 +29,19 @@ function EquipmentDetailScreen() {
         }
         addToCart(equipment);
         setAddedToCart(!addedToCard);
+    }
+
+    const handleDelete = async () => {
+        if (!equipment)
+            return;
+
+        if (addedToCard) {
+            removeFromCart(equipment?.id);
+            return;
+        }
+
+        await deleteEquipment(equipment.id);
+        navigate("/ShelfTracker/");
     }
 
     useEffect(() => {
@@ -67,6 +82,12 @@ function EquipmentDetailScreen() {
                         <Icon size={28} color="white" />
                     </div>
                     <h2 className="equipment-detail-name">{equipment?.name}</h2>
+                    <button
+                        className="equipment-detail-delete-btn"
+                        onClick={handleDelete}
+                    >
+                        <IoTrashOutline className="equipment-detail-delete-icon" size={24} color="lightcoral" />
+                    </button>
                     <button
                         className="equipment-detail-add-btn"
                         onClick={handleClick}
